f33f6fc34a75dde5291ad83994c809d9762e252f
N4EXM <naemmmail89@gmail.com>
2025-06-13 12:03:12 +0100

    added a warning box to CreateArticle page and added a category selection

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -10,13 +10,19 @@ const CreateArticle = () => {
         navigate(-1)
     }
 
-    const [isWarningBoxActive, setIsWarningBoxActive] = useState(false)
-    const [title, setTitle] = useState("")
-    const [content, setContent] = useState("")
-    const [selectedImage, setSelectedImage] = useState(null)
-    const [preview, setPreview] = useState(null)
+    const [title, setTitle] = useState("") // article title
+    const [content, setContent] = useState("")  // article content
+    const [selectedImage, setSelectedImage] = useState(null) // 
+    const [preview, setPreview] = useState(null) // preview the current image
     const fileInputRef = useRef(null); // Ref to access the file input
-    const textAreaRef = useAutoResizeTextarea(content)
+    const [isWarningBoxActive, setIsWarningBoxActive] = useState(false)
+    const textAreaRef = useAutoResizeTextarea(content) // textArea hook
+    const categories = [
+        "Tech",
+        "Finance",
+        "Sports",
+        "Gaming",
+    ]
 
     const handleImageChange = (e) => {
         e.preventDefault()
@@ -38,22 +44,31 @@ const CreateArticle = () => {
     };
 
   return (
-    <div className='w-full h-full p-5 flex flex-col gap-5 z-10 relative '>
+    <div className='w-full h-full p-5 flex flex-col gap-5 z-10 relative'>
         
-        {isWarningBoxActive ? 
+        {isWarningBoxActive &&
         
-            <div className=''>
+            <div className='border border-text/20 dark:border-darkText/20 fixed w-3/4 h-32 z-40 bg-background flex-col rounded-sm dark:bg-darkBackground flex justify-between left-1/2 top-1/2  transform -translate-x-1/2 -translate-y-1/2'>
+                
+                <div className='w-full h-fit gap-1 flex p-4 flex-col'>
+                    <h1 className='text-sm font-medium h-fit'>Are you sure you want to exit</h1>
+                    <p className='w-full h-fit text-xs opacity-60'>All your data will be lost</p>
+                </div>
+                
+                <div className='w-full h-full flex items-end'>
+                    <button onClick={() => setIsWarningBoxActive(false)} className='w-1/2 bg-secDarkBackground/5 font-medium dark:bg-secBackground/5 h-3/4 rounded-bl-sm'>
+                        Cancel
+                    </button>
+                    <button onClick={() => handleNavigate()} className='bg-rose-500 w-1/2 h-3/4 text-darkText font-medium rounded-br-sm'> 
+                        Exit
+                    </button>
+                </div>
 
             </div>
-            
-        :
-
-            <div></div>
-
         }
 
         {/* go back to previous page */}
-        <button onClick={() => handleNavigate()} className='p-2 rounded-full w-fit bg-secBackground dark:bg-secDarkBackground'>
+        <button onClick={() => setIsWarningBoxActive(true)} className='p-2 rounded-full w-fit bg-secBackground dark:bg-secDarkBackground'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"  
                 fill="currentColor" viewBox="0 0 24 24" >
                 <path d="M11.79 6.29 6.09 12l5.7 5.71 1.42-1.42L9.91 13H18v-2H9.91l3.3-3.29z"></path>
@@ -98,6 +113,17 @@ const CreateArticle = () => {
 
         }
 
+        {/* category selection */}
+        <div className='w-full h-fit flex flex-col gap-2 text-sm bg-secBackground dark:bg-secDarkBackground p-3 rounded-sm border border-text/20 dark:border-darkText/20'>
+            <select className=''>
+                {categories.map((category,_) => (
+                    <option key={category} value={category}>{category}</option>
+                ))}
+            </select>
+            <hr className='dark:text-darkText/20 text-text/20'/>
+        </div>
+        
+
         {/* title field */}
         <div className='w-full h-full flex flex-col gap-1'>
             <label 
@@ -113,7 +139,7 @@ const CreateArticle = () => {
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
                 maxLength={128}
-                className='w-full font-medium text-sm outline-none border-text/20 dark:border-darkText/20 border p-2 pl-3 rounded bg-secBackground dark:bg-secDarkBackground resize-none h-24'  
+                className='w-full font-medium text-sm outline-primary dark:outline-darkPrimary border-text/20 dark:border-darkText/20 border p-2 pl-3 rounded bg-secBackground dark:bg-secDarkBackground resize-none h-24'  
             />
         </div>
 
@@ -131,7 +157,7 @@ const CreateArticle = () => {
                 id='contentField'
                 onChange={(e) => setContent(e.target.value)}
                 ref={textAreaRef}
-                className='w-full min-h-52 h-full text-xs outline-none border-text/20 dark:border-darkText/20 border p-2  rounded bg-secBackground dark:bg-secDarkBackground resize-none '  
+                className='w-full min-h-52 h-full text-xs outline-primary dark:outline-darkPrimary  border-text/20 dark:border-darkText/20 border p-2 rounded bg-secBackground dark:bg-secDarkBackground resize-none '  
             />
         </div>
 
