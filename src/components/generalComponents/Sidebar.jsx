9088b0f5e994099d05a25af4bc526808396171c5
N4EXM <naemmmail89@gmail.com>
2025-07-22 15:43:12 +0100

    added some files

--- src/components/generalComponents/Sidebar.jsx
+++ src/components/generalComponents/Sidebar.jsx
@@ -105,12 +105,12 @@ const Sidebar = () => {
             <div className='w-full h-full flex justify-end items-end'>
                 <button 
                     onClick={() => handleNavigate("/CreateArticle")}
-                    className='flex flow-row items-center gap-2 bottom-5 rounded-md pl-3 text-sm bg-primary w-full h-fit p-2 dark:bg-darkPrimary font-medium'
+                    className='flex flow-row items-center justify-center gap-1 pr-5 bottom-5 rounded-md pl-3 text-sm bg-primary w-full h-fit p-2 dark:bg-darkPrimary font-medium'
                 >
-                <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16"  
-                    fill="currentColor" viewBox="0 0 24 24" >
-                    <path d="m19.41,3c-.78-.78-2.05-.78-2.83,0L4.29,15.29c-.13.13-.22.29-.26.46l-1,4c-.08.34.01.7.26.95.19.19.45.29.71.29.08,0,.16,0,.24-.03l4-1c.18-.04.34-.13.46-.26l12.29-12.29c.78-.78.78-2.05,0-2.83l-1.59-1.59Zm-11.93,15.1l-2.11.53.53-2.11L15,7.41l1.59,1.59-9.1,9.1Zm10.51-10.51l-1.59-1.59,1.59-1.59,1.59,1.58-1.59,1.59Z"></path>
-                </svg>
+                    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16"  
+                        fill="currentColor" viewBox="0 0 24 24" >
+                        <path d="m19.41,3c-.78-.78-2.05-.78-2.83,0L4.29,15.29c-.13.13-.22.29-.26.46l-1,4c-.08.34.01.7.26.95.19.19.45.29.71.29.08,0,.16,0,.24-.03l4-1c.18-.04.34-.13.46-.26l12.29-12.29c.78-.78.78-2.05,0-2.83l-1.59-1.59Zm-11.93,15.1l-2.11.53.53-2.11L15,7.41l1.59,1.59-9.1,9.1Zm10.51-10.51l-1.59-1.59,1.59-1.59,1.59,1.58-1.59,1.59Z"></path>
+                    </svg>
                     <p>Create article</p>
                 </button>
             </div>
